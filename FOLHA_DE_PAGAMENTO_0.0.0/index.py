7b246cef40e6e78d6651784fd09300da54d2666f
Marcos Serra <marcosserra133@gmail.com>
2023-03-06 23:10:53 -0300

    0.1.2

--- FOLHA_DE_PAGAMENTO_0.0.0/index.py
+++ FOLHA_DE_PAGAMENTO_0.0.0/index.py
@@ -1,76 +1,70 @@
  
-
-#valor_hora = float(input('Digite o valor da hora de trabalho (somente numeros): '))
-#horas_trabalhadas = float(input('Digite quantas horas trabalhada no mes (somente numeros): '))
-
-
-
-
-
 # Dados para calculo da folha
 
 salario_bruto = float(input('Digite um salario (somente numeros): ')) # Informar salario
 
-inss_desc = 0  # desconto de inss
+inss_aliquota = 0  # desconto de inss
 porcentagem_inss = 0 # porcentagem aplicada
 
-# descobrindo a aliquota
+# encontrando porcentagem da aliquota do inss
 
 if salario_bruto > 0 and salario_bruto <= 1302.00:
-    inss_desc = 0.075
+    inss_aliquota = 0.075
     porcentagem_inss = '7,5%'
 
 elif salario_bruto > 1302.01 and salario_bruto <= 2571.29:
-    inss_desc = 0.09
+    inss_aliquota = 0.09
     porcentagem_inss = '9%'
 
 elif salario_bruto > 2571.30 and salario_bruto <= 3856.94:
-    inss_desc = 0.12
+    inss_aliquota = 0.12
     porcentagem_inss = '12%'
 
 else:
-    inss_desc = 0.14
+    inss_aliquota = 0.14
     porcentagem_inss = '14%'
 
-valor_inss = salario_bruto * inss_desc # valor do inss dado pelo valor do salario bruto * desconto do inss
+valor_inss = salario_bruto * inss_aliquota # valor do inss dado pelo valor do salario bruto * desconto do inss
 
 base_irrf = salario_bruto - valor_inss # base irrf / ou imposto de renda
 
 
-# conseguindo o valor da aliquota do imposto de renda
+# encontrando porcentagem da aliquota do imposto de renda
 
-porcentagem_irrf = 0
+ir_aliquota = 0
+porcentagem_ir = 0
 
 if base_irrf > 0 and base_irrf <= 1903.98:
-    porcentagem_irrf = 0
+    ir_aliquota = 0
+    porcentagem_ir = '0'
 
 elif base_irrf > 1903.99 and base_irrf <= 2826.65:
-    porcentagem_irrf = 0.075
+    ir_aliquota = 0.075
+    porcentagem_ir = '7,5%'
 
 elif base_irrf > 2826.66 and base_irrf <=  3751.05:
-    porcentagem_irrf = 0.15
+    ir_aliquota = 0.15
+    porcentagem_ir = '15%'
 
 elif base_irrf > 3751.06 and base_irrf <= 4664.68:
-    porcentagem_irrf = 0.225
+    ir_aliquota = 0.225
+    porcentagem_ir = '22,5%'
 
 else:
-    porcentagem_irrf = 0.275
+    ir_aliquota = 0.275
+    porcentagem_ir = '27,5%'
 
 
-salario_liquido = salario_bruto - valor_inss
+salario_liquido = salario_bruto - valor_inss # valor do salario liquido
+fgts = salario_bruto * 0.08
+#total_descontos = valor_inss + 
 
 
-print(salario_bruto)
-print('----------------')
-print(porcentagem_inss)
-print('----------------')
-print(valor_inss)
-print('----------------')
-print(base_irrf)
-print('-----------------')
-print(porcentagem_irrf)
-print('-----------------')
-print(salario_liquido)
-
-
+print('>>>>>>>>>>>>>> FOLHA DE PAGAMENTO <<<<<<<<<<<<<<')
+print('Salario Bruto............................: R$%.2f' %(salario_bruto))
+print('(-)IR ('+porcentagem_ir+')...............: R$%.2f' %(ir_aliquota))
+print('(-)INSS ('+porcentagem_inss+')...........: R$%.2f' %(valor_inss))
+print('FGTS.....................................: R$%.2f' %(fgts))
+#print('Total de descontos.......................: R$%.2f' %())
+print('Salario Liquido...........................: R$%.2f' %(salario_liquido))
 
---
+++ FOLHA_DE_PAGAMENTO_0.0.0/sadd.py
@@ -0,0 +1,2 @@
+#valor_hora = float(input('Digite o valor da hora de trabalho (somente numeros): '))
+#horas_trabalhadas = float(input('Digite quantas horas trabalhada no mes (somente numeros): '))
